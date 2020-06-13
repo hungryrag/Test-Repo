5e108dd06b371fa993f8c79d85d05894b246eb4b
hungryrag <supriyojana6@gmail.com>
2020-06-14 03:14:42 +0530

    Removed some code

--- script.py
+++ script.py
@@ -1,16 +1,5 @@
-import sys
 import requests
 
-# print(sys.version)
-print(sys.executable)
 
-
-def greet(who_to_greet):
-    greetings = "Hello, {}".format(who_to_greet)
-    return greetings
-
-
-print(greet("World"))
-print(greet("Dhrubo"))
 r = requests.get("https://coreyms.com")
 print(r.status_code)
