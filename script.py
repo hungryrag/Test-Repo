85d16b62e94a3819ff76f96f3778f5d1117a4937
hungryrag <supriyojana6@gmail.com>
2020-06-26 23:33:16 +0530

    Created greetings() func

--- script.py
+++ script.py
@@ -1,5 +1,14 @@
 import requests
 
+def greetings(name):
+    greet = 'Hello {}, nice to meet you!'.format(name)
+    #greet = "Hello "+name+", nice to meet you!"
+    return greet
 
-r = requests.get("https://coreyms.com")
+r = requests.get("https://google.com")
 print(r.status_code)
+
+greet = greetings("Dhrubo")
+print(greet)
+
+
